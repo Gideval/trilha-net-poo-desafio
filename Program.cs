acae256e21c4a97662f46a363e0f113f45721dbc
Gideval <santosgideval@gmail.com>
2022-11-11 18:45:08 -0300

    Adicionando herança e e metodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smatphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "N2022", imei: "2222222222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smatphone Iphone:");
+Smartphone iphone = new Iphone(numero: "7891012", modelo: "IPH2022", imei: "3333333333", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
